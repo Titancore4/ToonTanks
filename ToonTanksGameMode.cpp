2e63aa96e5f30a3c83c0319db98d78735dfc373b
Jatin <hardevrag@gmail.com>
2023-03-02 13:12:09 +0530

    Set Player Controller

--- ToonTanksGameMode.cpp
+++ ToonTanksGameMode.cpp
@@ -2,4 +2,33 @@
 
 
 #include "ToonTanksGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tank.h"
+#include "Tower.h"
+#include "ToonTanksPlayerController.h"
+
+void AToonTanksGameMode::ActorDied(AActor *DeadActor)
+{
+    if (DeadActor == Tank)
+    {
+        Tank->HandleDestructino();
+        if (ToonTanksPlayerController)
+        {
+            ToonTanksPlayerController->SetPlayerEnabledState(false);
+        }
+        
+    }
+    else if (ATower* DestroyedTower = Cast<ATower>(DeadActor))
+    {
+        DestroyedTower->HandleDestruction();
+    }
+}
+
+void AToonTanksGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+}
 
